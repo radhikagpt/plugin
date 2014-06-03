fc477def71536e3fb022013b9b0a6f588bb158c5
radhikagpt <radhikaosiya01@gmail.com>
2014-06-03 16:48:54 +0530

    Update test.js

--- book/test.js
+++ book/test.js
@@ -1,6 +1,7 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("page.change", function() {
         // do something
+        alert('hello');
     });
 
     gitbook.events.bind("exercise.submit", function() {
